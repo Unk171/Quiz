3bed6a5dd76d97aa4bef6f8e445b0963bfd1b471
Dmitry Dmitry <7353687@gmail.com>
2023-06-25 12:15:21 -0400

    added part of html frame and quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+
+// www.tutorialspoint.com
+
+// Q 1 - Which of the following is true about variable naming conventions in JavaScript?
+
+// A - JavaScript variable names must begin with a letter or the underscore character.
+// B - JavaScript variable names are case sensitive.
+// C - Both of the above.!
+// D - None of the above.
+
+
+// Q 2 JavaScript File Has An Extension of:
+// A - .Java
+// B - .Js
+// C - .javascript
+// D - .xml
+
+
+// Q 3 - Which built-in method adds one or more elements to the end of an array and returns the new length of the array?
+// A - last()
+// B - put()
+// C - push()!
+// D - None of the above.
+
+
+// Q 4 - Which built-in method returns the calling string value converted to lower case?
+// A - toLowerCase()!
+// B - toLower()
+// C - changeCase(case)
+// D - None of the above.
+
+
+// Q 5 - Which of the following function of Number object returns the number's value?
+// A - toString()
+// B - valueOf()!
+// C - toLocaleString()
+// D - toPrecision()
+
+
+// Q 6 - Which of the following function of String object is used to match a regular expression against a string?
+// A - concat()
+// B - match()!
+// C - search()
+// D - replace()
+
+
+// Q 7 - Which of the following function of String object creates a string to be displayed in a big 
+// font as if it were in a <big> tag?
+// A - anchor()
+// B - big()!
+// C - blink()
+// D - italics()
+
+
+// Q 8 - Which of the following function of String object creates an HTML hypertext link that requests another URL?
+// A - link()!
+// B - sub()
+// C - sup()
+// D - small()
+
+
+// Q 9 - Which of the following function of Array object removes the last element from an array and returns that element?
+// A - pop()!
+// B - push()
+// C - join()
+// D - map()
+
+
+// Q 10 - Which of the following function of Array object adds and/or removes elements from an array?
+// A - toSource()
+// B - sort()
+// C - splice()!
+// D - unshift()
+
+
